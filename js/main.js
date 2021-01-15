40f3d3f63dcb3cfad007078b086745a7903baf34
adele <adele.r87@gmail.com>
2021-01-15 17:37:52 +0100

    modifiche

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 // Un alert espone 5 numeri casuali diversi.
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
-// Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzae il programma.
 
 //1 --- alert espone 5 numeri casuali diversi
 // funzioni che mi genera numri casuali
@@ -28,28 +24,33 @@ function randomNum(min,max) {
 }alert('prova a ricordare questi numeri' + ' ' + arrayNum);
 
 
-
-
-
   // Dopo 30 secondi l’utente deve inserire,
   setTimeout(function() {
   var arrayUtente = []
   while (arrayUtente.length <  howManyNumber) {
   var numeriUtente = parseInt(prompt('inserisci i numeri che ti ricordi'))
   arrayUtente.push(numeriUtente)
-   console.log('i numeri che hai inserito sono i seguenti ' + arrayUtente);
 
+}
+console.log('i numeri che hai inserito sono i seguenti ' + arrayUtente);
 
  // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
- var arrayNumeriRicordati= []
 
- if (arrayNum.includes(numeriUtente)){
-   arrayNumeriRicordati.push(numeriUtente)
- }
- console.log('hai ricordato questi numeri' + arrayNumeriRicordati + 'ed il tuo punteggio è' +  arrayNumeriRicordati.length);
- }
+ var arrayNumeriRicordati = []
+
+  for (var i = 0; i < arrayNum.length; i++) {
+    if (arrayNum.includes(numeriUtente)){
+      arrayNumeriRicordati.push(numeriUtente)
+    }
+  }
+
+ console.log( 'i numeri che hai ricordato correttamente sono ' + ' ' +  arrayNumeriRicordati);
+
+ console.log('hai ricordato questi numeri' + arrayNumeriRicordati + ' ' + 'ed il tuo punteggio è' +  arrayNumeriRicordati.length);
+
 }, 5000)
+
 
 
  // un prompt alla volta, i numeri che ha visto precedentemente.
