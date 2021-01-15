34442c4af160d2d3ed76bc1d4ac4d3bdff65d1e5
adele <adele.r87@gmail.com>
2021-01-15 15:04:35 +0100

    inizio

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+// Consigli del giorno:
+// * Pensate prima in italiano.
+// * Dividete in piccoli problemi la consegna.
+// * Individuate gli elementi di cui avete bisogno per realizzae il programma.
+
+//1 --- alert espone 5 numeri casuali diversi
+// funzioni che mi genera numri casuali
+var min = 1;
+var max = 100;
+
+function randomNum(min,max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min)
+ }
+
+
+
+ // voglio 5 numeri casuali li metto all interno del while e li popolo in un array
+
+ var arrayNum = []
+ var howManyNumber = 5
+
+ while ( arrayNum.length < howManyNumber) {
+   var randomNumber = randomNum(min,max)
+   arrayNum.push(randomNumber)
+}
+  alert('prova a ricordare questi numeri' + ' ' + arrayNum);
+
+
+  // Dopo 30 secondi l’utente deve inserire,
+
+ // un prompt alla volta, i numeri che ha visto precedentemente.
+
+ var arrayUtente = []
+ while (arrayUtente.length <  howManyNumber) {
+ var numeriUtente = parseInt(prompt('inserisci i numeri che ti ricordi'))
+ arrayUtente.push(numeriUtente)
+}console.log('i numeri che hai inserito sono i seguenti ' + arrayUtente);
